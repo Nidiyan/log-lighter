68c4a757257b9d55e86d1507953d317103b788b3
nidiyan <nidiyan@gmail.com>
2023-02-15 13:38:21 -0600

    Added contribution.viewContainers for new icon for log-lighter

--- src/extension.ts
+++ src/extension.ts
@@ -21,7 +21,6 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from log-lighter!');
-		LogLighterPanel.render();
 
 		const openEditor = vscode.window.visibleTextEditors.filter(editor => editor.document.uri)[0];
 
